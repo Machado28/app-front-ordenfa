59b387266a0012d632b276d5dceebf9bf713bac2
Machado28 <ulundoantonio@gmail.com>
2022-04-11 12:34:25 +0100

    clean

--- src/pages/minhaCarteira/index.js
+++ src/pages/minhaCarteira/index.js
@@ -5,7 +5,6 @@ import Barner from "../../components/barner";
 import Footer from "../../components/footer";
 import Container,{ FormContainer } from "../styled";
 import { Slider } from "./data";
-import ModalDesconto from "../../components/ModalDescontoDirecto";
 
 const MinhaCarteira = () => {
   return (
